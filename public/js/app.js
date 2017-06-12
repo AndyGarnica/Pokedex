6a7ac4c365f062ece801d8983bc6a9d8540ac5b5
Elsa Andrea Garnica Reyes <elsaandreagarnicareyes@MacBook-Air-de-Elsa.local>
2017-06-12 16:24:36 -0500

    funcionalidad de modal

--- public/js/app.js
+++ public/js/app.js
@@ -55,11 +55,11 @@ var imagenes = [
 	},
 ];
 var plantilla = "<div class='col s3 card tarjeta hoverable'>"+
-					'<a href="#modalPokemon" class="link" data-url="url" >' +
-						'<img src="imagen" class="center-align img">' +
+					'<a href="#modalPokemon" class="link" data-url="__url__" >' +
+						'<img src="__imagen__" class="center-align img">' +
 					'</a>'+
-					'<h6 class="center-align">' +
-						'nombrePokemon'+ '</h6>' +
+					'<h5 class="center-align">' +
+						'__nombrePokemon__'+ '</h5>' +
 					'</div>';
 
 $.getJSON("http://pokeapi.co/api/v2/pokemon/", function(response){
@@ -73,24 +73,24 @@ function crearPokemons(pokemons, imagenes) {
 	var plantillaFinal = "";
 	var noPokemon;
 	pokemons.forEach(function (pokemon, indice) {
-		plantillaFinal += plantilla.replace("nombrePokemon", pokemon.name).replace("imagen", imagenes[indice].imagen)
-		.replace("url", "http://pokeapi.co/api/v2/pokemon-species/"+(indice+1)+"/");
+		plantillaFinal += plantilla.replace("__nombrePokemon__", pokemon.name).replace("__imagen__", imagenes[indice].imagen)
+		.replace("__url__", "http://pokeapi.co/api/v2/pokemon-species/"+(indice+1)+"/");
   	});
 	$contPokemones.html(plantillaFinal);
 	var $link = $('.link').click(detallePokemon);
 	modalPokemon();
 }
 
-var plantillaDetalle = '<h4>nombre</h4>' +
+var plantillaDetalle = '<h5 class="center-align">__nombre__</h5>' +
 							'<div class="row">' +
 								'<div class="col s6">'+
-									'<img src="src" class="img-100 vertical-align">'+
+									'<img src="__src__" class="img-100 vertical-align">'+
 								'</div>'+
 								'<div class="col s6">'+
-									'<p><strong>Habitat :</strong>habitat</p>'+
-									'<p><strong>Color :</strong>color</p>'+
-									'<p><strong>Shape :</strong>shape</p>'+
-									'<p><strong>Genera :</strong>genera</p>'+
+									'<p><strong>Habitat :</strong>__habitat__</p>'+
+									'<p><strong>Color :</strong>__color__</p>'+
+									'<p><strong>Shape :</strong>__shape__</p>'+
+									'<p><strong>Genera :</strong>__genera__</p>'+
 								'</div>'+
 							'</div>'+
 						'</div>';
@@ -99,7 +99,7 @@ function detallePokemon(){
 	console.log(this);
 	var $imagen = $(this).find("img").attr('src');
 	console.log($imagen);
-	var $nombre = ($(this).parent().children('h6').text()).toUpperCase();
+	var $nombre = ($(this).parent().children('h5').text()).toUpperCase();
 	console.log($nombre);
 	console.log(this.dataset.url);
 	$('.modal-content').html("");
@@ -123,12 +123,12 @@ function detallePokemon(){
 function crearDetalle(detalle){
 	var $modalDetalle = $('.modal-content');
 	// console.log(habitat, color, shape, genera, imagen, nombre);
-	var nuevaPlantilla = plantillaDetalle.replace("nombre", detalle.nombre)
-		.replace("habitat", detalle.habitat)
-		.replace("color", detalle.color)
-		.replace("shape", detalle.shape)
-		.replace("genera", detalle.genera)
-		.replace("src", detalle.imagen);
+	var nuevaPlantilla = plantillaDetalle.replace("__nombre__", detalle.nombre)
+		.replace("__habitat__", detalle.habitat)
+		.replace("__color__", detalle.color)
+		.replace("__shape__", detalle.shape)
+		.replace("__genera__", detalle.genera)
+		.replace("__src__", detalle.imagen);
 
 	$modalDetalle.html(nuevaPlantilla);
 }
